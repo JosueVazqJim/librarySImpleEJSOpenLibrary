e2fd7d6c65c3ecbc9e7b5a6fd422b4b8dfaadf78
JosueVJ <josuevj.1e.49@gmail.com>
2024-09-06 20:38:31 -0600

    update number of request

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import rateLimit from "express-rate-limit";
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 120 // límite de 100 solicitudes por IP cada 15 minutos
+  max: 150 // límite de 100 solicitudes por IP cada 15 minutos
 });
 
 const app = express();
